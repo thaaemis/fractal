8bdc38127bda5d1447f97258bacc529d25d596e7
thaaemis <bfkraus@gmail.com>
2016-01-26 11:06:35 -0500

    First attempt at fractal code
    Mechanisms to plot regions, deal with overlap in rudimentary way.

---
+++ fractal.py
@@ -0,0 +1,102 @@
+from pylab import *
+
+class window:
+    def __init__(self,n,m,d,k):
+        self.r = float(n)/float(m) # rational center
+        self.num = n # numerator
+        self.den = m # denominator
+        self.low  = (n * m**(k-1) - d)/m**k # excluded upper bound
+        self.high = (n * m**(k-1) + d)/m**k # excluded low bound
+        
+    def __lt__(self,other):
+        return self.r < other.r
+        
+    def checkOverlap(self,other):
+        if self.r < other.r:
+            return self.high > other.low
+        else:
+            return self.low < other.high
+        
+    def totalOverlap(self,other):
+        return (self.high < other.high) and (other.low < self.low)
+        
+    def merge(self,other):
+        low = min(self.low,other.low)
+        high = max(self.high,other.high)
+        other.low = low
+        other.high = high
+        return other
+        
+    def printFrac(self):
+        print self.num,"/",self.den
+        
+
+def farey( n, asc=True ):
+    """Python function to print the nth Farey sequence, either ascending or descending."""
+    row = set()
+    if asc: 
+        a, b, c, d = 0, 1,  1 , n     # (*)
+    else:
+        a, b, c, d = 1, 1, n-1, n     # (*)
+    row.add((a,b))
+    while (asc and c <= n) or (not asc and a > 0):
+        k = int((n + b)/d)
+        a, b, c, d = c, d, k*c - a, k*d - b
+        row.add((a,b))
+    return row
+    
+def getWindows(n,d,k,plotYN=True):
+    rationals = farey(n)
+    windows = []
+    for x in rationals:
+        a = window(x[0],x[1],d,k)
+        windows.append(a)
+        if plotYN:
+            plot(a.r,n,'ro',a.low,n,'k<',
+            a.high,n,'k>',ms=15./x[1])
+    return windows
+
+def plotRegions(d,k,nmax,plotYN=True):
+    windowSets = []
+    for n in range(1,nmax+1):
+        a = getWindows(n,d,k,plotYN)
+        windowSets.append(a)
+    if plotYN:
+        xlim(xmin=-0.1,xmax=1.1)
+        ylim(ymin=0,ymax=nmax+1)
+        show()
+
+def main(d,k,nmax):
+    # Define initial regions.
+    regions = [window(0,1,d,k),window(1,1,d,k)]
+    
+    # Loop over levels of Farey tree from 2 until nmax:
+    for n in range(2,nmax+1):
+        # Compute new Farey rationals
+        newregions = list(farey(n)-farey(n-1))
+        regionsCopy = regions
+        # Check what to do with new window around this rational.
+        for x in newregions:
+            addBool = True
+            newWindow = window(x[0],x[1],d,k)
+            for y in regionsCopy:
+                if not newWindow.checkOverlap(y):
+                    continue
+                elif newWindow.totalOverlap(y):
+                    addBool = False
+                    break
+                else:
+                    regions.remove(y)
+                    newWindow = newWindow.merge(y)
+                    break
+            if addBool:
+                regions.append(newWindow)
+        regions.sort()
+        
+    print(len(regions))
+    plotRegions(d,k,nmax,True)
+    
+main(0.1,2,5)
+main(1,2,5)
+main(.3,1.5,5)
+                
