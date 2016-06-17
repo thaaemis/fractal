5bbce5c0885ee90a0499ef6989a9aef72babfd6e
thaaemis <bfkraus@gmail.com>
2016-06-17 14:30:59 -0400

    Object oriented structure to self-Similar
    Substantial restructuring to code to allow for programmed repeating
    LRLRLR patterns, as well as "honing in" on a number like 1/3 or some
    irrational. Still need to fix problem with "median" not being an
    accurate measurement of either asymptotic center or fraction.

--- selfSimilar.py
+++ selfSimilar.py
@@ -1,79 +1,121 @@
 from pylab import *
 import numpy as np
 
-def main(LR=True, a = 1, plotOn=False):
-    fareyNum = [1., 1.] # initial conditions
-    fareyDen = [1., 2.] # for left side of tree
-    diophantine = [] # edge of relevant excluded region
-    fraction = [] # size of remaining region / size between rationals
-    d, k = 0.2, 2
+def main(path, plotOn=False, d = 0.2, k = 2, col = 'k'):
 
-    for n in range(2, 100):
-        if LR:
-            fareyNum.append(np.float64(fareyNum[n-2]+fareyNum[n-1]))
-            fareyDen.append(np.float64(fareyDen[n-2]+fareyDen[n-1]))
-        else:
-            fareyNum.append(np.float64(fareyNum[n-1]+1.))
-            fareyDen.append(np.float64(fareyDen[n-1]+np.float64(a)))
+    class rational:
+        def __init__(self,num,den):
+            self.num = int(num)
+            self.den = int(den)
+            self.val = float(num)/float(den)
+            self.width = d/self.den**k
+            self.diophantine = (self.val - self.width, self.val + self.width)
+        
+        def __lt__(self,other):
+            return self.val < other.val
             
-    rationals = np.array(fareyNum)/np.array(fareyDen)
-    center = np.median(rationals[-5:-1])
-    # center = 2/(1+math.sqrt(5))
-    print(center)
+        def __sub__(self,other):
+            return self.val - other.val
+            
+        def __repr__(self):
+            return str(self.num)+'/'+str(self.den)
 
-    # Get Diophantine windows
-    for i in range(len(fareyNum)):
-        window = np.float64(d/fareyDen[i]**k)
-        edge = rationals[i] + window if rationals[i] < center else \
-            rationals[i] - window
-        diophantine.append(edge)
-        if i >= 1:
-            wholeSize = np.float64(abs(rationals[i] - rationals[i-1]))
-            leftSize = abs(diophantine[i]-diophantine[i-1])
-            fraction.append(np.float64(leftSize/wholeSize))
-    fraction = np.array(fraction)
-    # if plotOn:
-        # figure(1)
-        # subplot(2,1,1)
-        # if LR:
-            # plot(fraction)
-        # else:
-            # plot(fraction,'r')
-        # text(3,1.02,'Remaining fraction between two rational excluded regions')
-        # xticks([])
-        # ylabel(r"$f_L$")
+    def getFraction(farey1, farey2):
+        separation = abs(farey1 - farey2)
+        widthLeft = min(abs(farey1.diophantine[0] - farey2.diophantine[1]), 
+                        abs(farey1.diophantine[1] - farey2.diophantine[0]))
+        try:
+            fraction = widthLeft / separation
+        except ZeroDivisionError:
+            fraction = 0
+        return fraction
+
+    farey = [rational(0,1), rational(1, 1), rational(1,2)]
+    fraction = [] # size of remaining region / size between rationals
+    maxN = 45
+    if type(path) == str:
+        path = path*int((maxN+1)/len(path))
+        target = None
+    else:
+        target = path
+        path = ''
+    
+    def nextLevel(farey1, farey2):
+        num, den = farey1.num + farey2.num, farey1.den + farey2.den
+        return rational(num,den)
+
+    # Generate Farey tree in appropriate path/number
+    for n in range(2, maxN):
+        if type(target) == (float or int):
+            if target > farey[-1].val:
+                path = path + 'R' 
+            else:
+                path = path + 'L'
+        if path[n-2] == 'L':
+            relevantFarey = [x for x in farey if x < farey[-1]]
+        elif path[n-2] == 'R':
+            relevantFarey = [x for x in farey if x > farey[-1]]
+        winner, minDist = relevantFarey[0], abs(farey[-1]-relevantFarey[0])
+        for i in range(1,len(relevantFarey)):
+            if abs(farey[-1]-relevantFarey[i]) < minDist:
+                winner = relevantFarey[i]
+                minDist = abs(farey[-1]-relevantFarey[i])
         
-        # subplot(2,1,2)
-        # if LR:
-            # semilogy(abs(fraction-np.mean(fraction)))
-        # else:
-            # semilogy(abs(fraction-np.mean(fraction)),'r')
+        farey.append(nextLevel(farey[-1],winner))
+
+    if type(target) == (float or int):
+        center = target
+    else:
+        center = np.mean([x.val for x in farey[-5:-1]])
+    
+    print(center)
+    
+    # Get irrational fraction between two rationals
+    fraction = []
+    for i in range(1,len(farey)):
+        fraction.append(getFraction(farey[i-1],farey[i]))
+    fraction = np.asarray(fraction)
+    
+    if plotOn:
+        figure(1)
+        subplot(2,1,1)
+        plot(fraction,color=col)
+        text(3,1.02,'Remaining fraction between two rational excluded regions')
+        xticks([])
+        ylabel(r"$f_L$")
+        
+        subplot(2,1,2)
+        semilogy(abs(fraction-np.median(fraction)),color=col)
         # legend(['Alternating noble steps',r"Steps approaching 1/3"],loc='best')
-        # xlabel('Steps down Farey tree')
-        # ylabel(r"Error from median $f_L$")
-        # subplots_adjust(hspace=0)
+        xlabel('Steps down Farey tree')
+        ylabel(r"Error from asymptotic $f_L$")
+        subplots_adjust(hspace=0)
         
-    diophantine = np.array(diophantine)
     # Plot excluded windows visually
     if plotOn:
         figure(2)
-        if LR:
-            plot(abs(rationals-center),range(len(rationals)),'ko')
-            plot(abs(diophantine-center),range(len(rationals)),'bx')
-            
-        else:
-            plot(abs(rationals-center),range(len(rationals)),'ro')
-            plot(abs(diophantine-center),range(len(rationals)),'rx')
-        xscale('log')
-        legend(['Rational center','Diophantine window edge'],loc='best')
-        xlabel('Distance from asymptotic limit')
-        ylabel('Steps down Farey tree')
-        
-main(True,1,True)
-main(False,1,True)
-main(False,2,True)
-main(False,3,True)
+        for i in range(len(farey)):
+            plot(i,abs(farey[i].val-center),color=col,marker='o')
+            plot(i,min(abs(farey[i].diophantine[0]-center),abs(farey[i].diophantine[1]-center)),
+                    color=col,marker='x')
+        yscale('log')
+        legend(['Rational','Diophantine window edge'],loc='best')
+        ylabel('Distance from asymptotic limit')
+        xlabel('Steps down Farey tree')
 
-# main(7,8)
+dmin, dmax = 0.15, 0.35
+cmap = cm.get_cmap('gnuplot2')
+junk = contourf([[0,0],[0,0]],np.arange(dmin,dmax+0.01,0.001),cmap=cmap)
+clf()
 
+# for d in np.arange(dmin, dmax, 0.01): 
+#    main(True,1,True, d, cmap((d-dmin)/(dmax-dmin)))
+main('RLLL',True)
+main('RLL',True)
+main('RL',True)
+# main(1/math.pi,True)
+fig = figure(1)
+fig.subplots_adjust(right=0.8)
+cbarAx = fig.add_axes([0.85,0.15,0.05,0.7])
+colorbar(junk,cax=cbarAx)
 show()
